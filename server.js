0cfa55ab23704a3799a08f6f20e789289d63e49a
Daniil Rudakov <dannikryd@mail.ru>
2022-09-19 21:46:14 +0300

    запуск проекта

---
+++ server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer();
+
+server.on('request', (request, response)=>{
+    function sendFile(type){
+        let path = request.url.slice(1);
+        let file = fs.readFileSync(path, "utf8");
+
+        response.writeHead(200, {'Content-Type': type});
+        response.end(file);
+    }
+    switch(request.url){
+        case '/':
+            let index = fs.readFileSync("index.html", "utf8");
+
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(index);
+            break;
+        case '/css/style.css':
+            sendFile('text/css');
+            break;
+        case '/js/script.js':
+            sendFile('text/plain');
+            break;
+        default:
+            response.writeHead(404, {'Content-Type': 'text/plain'});
+            response.end('404 File Not Found');
+    }
+    
+})
+
+server.listen(8080, ()=>{console.log('Server works at localhost:8080')});
