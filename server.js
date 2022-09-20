3b6665e7d0d5cb1ca8d990aebe742e75ef9f2671
Daniil Rudakov <dannikryd@mail.ru>
2022-09-21 02:15:08 +0300

    генерация ссылки и шифрование данных при помощи crypto

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const http = require('http');
 const fs = require('fs');
-const server = http.createServer();
+const { publicDecrypt } = require('crypto');
+let crypto;
+try {
+     crypto = require('crypto');
+  } catch (err) {
+    console.log('Модуль Crypto недоступен');
+    process.exit(1);
+}
+const key = crypto.randomBytes(16).toString('hex');
 
+const server = http.createServer();
 server.on('request', (request, response)=>{
     function sendFile(type){
         let path = request.url.slice(1);
@@ -29,12 +38,19 @@ server.on('request', (request, response)=>{
                 data += chunk;
             });
             request.on("end", () => {
-                //сделать криптографию
                 let text = JSON.parse(data).text;
-                console.log(text);
-                response.writeHead(200, {'Content-Type': 'application/json'});
-                let link = '';
-                response.end(JSON.stringify({link: link}));
+                const encrypted = encrypt(text);
+
+                if (encrypted.length > 65535){
+                    response.writeHead(200, {'Content-Type': 'application/json'});
+                    response.end(JSON.stringify({result: 'fail', message: 'Строка слишком длинная'}));
+                }
+
+                let link = createLink();
+                let [hash, iv] = link[1].split('.');
+
+                //сохранить хэш в бд
+                response.end(JSON.stringify({result: 'ok', link: link[0] + '.' + iv}));
             });
             
             break;
@@ -44,5 +60,26 @@ server.on('request', (request, response)=>{
     }
     
 })
+
+function encrypt(str, iv = null){
+    iv = iv || crypto.randomBytes(8).toString('hex');
+    const cipher = crypto.createCipheriv('aes256', key, iv)
+    let encrypted = cipher.update(str, 'utf8', 'hex');
+
+    return encrypted + cipher.final('hex') + '.' + iv;
+}
+function decrypt(str){
+    const [encrypted, iv] = str.split('.');
+    const decipher = crypto.createDecipheriv('aes256', key, iv);
+    let decrypted = decipher.update(encrypted, 'hex', 'utf8')
+
+    return decrypted + decipher.final('utf8');
+}
+
+function createLink(){
+    let link = crypto.randomBytes(5).toString('hex');
+
+    return [link, encrypt(link)];
+}
 
 server.listen(8080, ()=>{console.log('Server works at localhost:8080')});
